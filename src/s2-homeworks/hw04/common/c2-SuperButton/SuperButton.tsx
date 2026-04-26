19caf34308704b983f6872ed87693727aba680a1
Mark <mazur.mark7002@gmail.com>
2026-04-25 20:42:50 -0400

    hw4

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -18,8 +18,7 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
     }
 ) => {
     const finalClassName = s.button
-        + (disabled ? ' ' + s.disabled : xType === 'red' ? ' ' + s.red : ' ' + s.default)
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+        + disabled ? ' ' + s.disabled : xType === 'red' ? ' ' + s.red : xType === 'secondary' ? ' ' + s.secondary : ' ' + s.default
 
     return (
         <button
